7b84c739893e8c58858daf8c51b916f18c051d04
Lucas Loiolla Gomes <loiollalucas@gmail.com>
2026-04-30 15:34:18 -0300

    pastas e configurações iniciais

--- src/main.ts
+++ src/main.ts
@@ -1,43 +1,33 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import { IonicVue } from '@ionic/vue'
+import { Storage } from '@ionic/storage'
 import App from './App.vue'
-import router from './router';
+import router from './router'
 
-import { IonicVue } from '@ionic/vue';
+import '@ionic/vue/css/core.css'
+import '@ionic/vue/css/normalize.css'
+import '@ionic/vue/css/structure.css'
+import '@ionic/vue/css/typography.css'
+import '@ionic/vue/css/padding.css'
+import '@ionic/vue/css/float-elements.css'
+import '@ionic/vue/css/text-alignment.css'
+import '@ionic/vue/css/text-transformation.css'
+import '@ionic/vue/css/flex-utils.css'
+import '@ionic/vue/css/display.css'
+import './theme/variables.css'
 
-/* Core CSS required for Ionic components to work properly */
-import '@ionic/vue/css/core.css';
+const storage = new Storage()
 
-/* Basic CSS for apps built with Ionic */
-import '@ionic/vue/css/normalize.css';
-import '@ionic/vue/css/structure.css';
-import '@ionic/vue/css/typography.css';
+storage.create().then(() => {
+  const app = createApp(App)
+    .use(IonicVue)
+    .use(createPinia())
+    .use(router)
 
-/* Optional CSS utils that can be commented out */
-import '@ionic/vue/css/padding.css';
-import '@ionic/vue/css/float-elements.css';
-import '@ionic/vue/css/text-alignment.css';
-import '@ionic/vue/css/text-transformation.css';
-import '@ionic/vue/css/flex-utils.css';
-import '@ionic/vue/css/display.css';
+  app.provide('storage', storage)
 
-/**
- * Ionic Dark Mode
- * -----------------------------------------------------
- * For more info, please see:
- * https://ionicframework.com/docs/theming/dark-mode
- */
-
-/* @import '@ionic/vue/css/palettes/dark.always.css'; */
-/* @import '@ionic/vue/css/palettes/dark.class.css'; */
-import '@ionic/vue/css/palettes/dark.system.css';
-
-/* Theme variables */
-import './theme/variables.css';
-
-const app = createApp(App)
-  .use(IonicVue)
-  .use(router);
-
-router.isReady().then(() => {
-  app.mount('#app');
-});
+  router.isReady().then(() => {
+    app.mount('#app')
+  })
+})
